71babf779180c51db25abf3d96fddc5844e5e183
Karl Hepler <karl.hepler@gmail.com>
2022-11-28 10:53:57 -0500

    it works

--- home.nix
+++ home.nix
@@ -6,6 +6,12 @@
   home.username = "karlhepler";
   home.homeDirectory = "/Users/karlhepler";
 
+  # Home Packages
+  # https://search.nixos.org/packages
+  home.packages = with pkgs; [
+    cowsay
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
@@ -18,4 +24,40 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Program Modules
+  # https://nix-community.github.io/home-manager/options.html
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      vim-surround
+      vim-signify
+      vim-pasta
+      auto-pairs
+      vim-commentary
+      {
+        plugin = vim-vinegar;
+	      config = "let g:netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'";
+      }
+      vim-repeat
+      vim-fugitive
+      {
+        plugin = vim-polyglot;
+        config = ''
+          let g:vim_json_syntax_conceal = 0
+          let g:vim_markdown_conceal = 0
+        '';
+      }
+    ];
+    
+    extraConfig = ''
+      let mapleader = ','
+      imap jk <esc>
+    '';
+  };
 }
