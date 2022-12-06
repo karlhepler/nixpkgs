d2ab52fc62784b63eeadc5b02fc0e85f7afe0f6a
Karl Hepler <karl.hepler@gmail.com>
2022-12-06 13:11:24 -0500

    added emmet-vim

--- home.nix
+++ home.nix
@@ -285,6 +285,12 @@ in rec {
           let g:prettier#autoformat_require_pragma = 0
         '';
       }
+      {
+        plugin = emmet-vim;
+        config = ''
+          let g:user_emmet_leader_key='<c-e>'
+        '';
+      }
     ];
     
     extraConfig = ''
