909d32d928332f0a2e1c855dac1a3d692543d49b
Karl Hepler <karl.hepler@gmail.com>
2024-10-25 12:18:59 -0400

    noop commits and rustfmt autosave

--- home.nix
+++ home.nix
@@ -9,8 +9,13 @@ let
       name = "commit";
       runtimeInputs = [ pkgs.git ];
       text = ''
-        git add "$(git rev-parse --show-toplevel)"
-        git commit -m "$*"
+        msg="$*"
+        if [ "$msg" == 'noop' ]; then
+          git commit --allow-empty -m "$msg"
+        else
+          git add "$(git rev-parse --show-toplevel)"
+          git commit -m "$msg"
+        fi
       '';
     };
     pull = pkgs.writeShellApplication {
