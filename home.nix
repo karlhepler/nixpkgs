e95a2f7415ee68567ff8c5c7867045679c9a0999
Karl Hepler <karl.hepler@gmail.com>
2025-12-08 22:35:52 -0500

    feat: Extract git config to modules/git.nix
    - Move git configuration from home.nix to modules/git.nix
    - Reduces home.nix by ~23 lines
    - Test successful with hms --dry-run

--- home.nix
+++ home.nix
@@ -618,30 +618,6 @@ in {
     enableZshIntegration = true;
   };
 
-  programs.git = {
-    enable = true;
-    ignores = [ ".DS_Store" ".tags*" ".claude/settings.local.json" ];
-    settings = {
-      user = {
-        name = "Karl Hepler";
-        email = "karl.hepler@gmail.com";
-      };
-      core.editor = "vim";
-      diff.tool = "vimdiff";
-      merge.tool = "vimdiff";
-      difftool.prompt = false;
-      push.default = "current";
-      init.defaultBranch = "main";
-      pull.rebase = false;
-      alias = {
-        who = "blame -w -C -C -C";
-        difft = "-c diff.external=difft diff";
-        logt = "-c diff.external=difft log -p --ext-diff";
-        showt = "-c diff.external=difft show --ext-diff";
-      };
-    };
-  };
-
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/neovim.nix
   programs.neovim = {
     enable = true;
--- modules/git.nix
+++ modules/git.nix
@@ -1,5 +1,27 @@
 { config, pkgs, lib, theme, ... }:
 
 {
-  # Git configuration will be moved here
+  programs.git = {
+    enable = true;
+    ignores = [ ".DS_Store" ".tags*" ".claude/settings.local.json" ];
+    settings = {
+      user = {
+        name = "Karl Hepler";
+        email = "karl.hepler@gmail.com";
+      };
+      core.editor = "vim";
+      diff.tool = "vimdiff";
+      merge.tool = "vimdiff";
+      difftool.prompt = false;
+      push.default = "current";
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      alias = {
+        who = "blame -w -C -C -C";
+        difft = "-c diff.external=difft diff";
+        logt = "-c diff.external=difft log -p --ext-diff";
+        showt = "-c diff.external=difft show --ext-diff";
+      };
+    };
+  };
 }
